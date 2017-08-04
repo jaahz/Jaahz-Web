7645ff75349017129844bdfd4fb8c210406efcab
jaahz <zanvier123@gmail.com>
2017-08-04 16:09:19 +0400

    Add files via upload

---
+++ Portfolio-Updated/contactForm.js
@@ -0,0 +1,10 @@
+function displayDetails(){
+var name= document.getElementById('text1').value;
+alert("Thank you for contacting us " + name);
+}
+
+function clearForm(){
+ document.querySelector("#contact").reset();
+}
+
+
